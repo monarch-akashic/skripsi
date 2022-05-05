9aa42d44d0c30cbe37e7d1f6bf4d3c8e46c6eafb
Virnando Tan Wijaya <nandovtw109@gmail.com>
2022-05-06 00:18:19 +0700

    Add Layout Styling

--- myscript.sh
+++ myscript.sh
@@ -9,3 +9,16 @@ npm audit fix
 npm run dev
 php artisan migrate:fresh
 php artisan storage:link
+
+git init
+git add .
+git status
+git remote add origin https://github.com/monarch-akashic/skripsi.git
+git config --global user.email "nandovtw109@gmail.com"
+git config --global user.name "Virnando Tan Wijaya"
+git commit -m 'First Commit'
+git status
+git push -u origin master
+
+php artisan make:model Applicant -m
+php artisan make:model Portofolio -m
